592295768a0d0d990002ac4272773e4d361a6d77
Christina Bui <uyen.buitie@gmail.com>
2023-02-13 07:54:32 -0600

    Movies

--- src/Components/Services/Information.js
+++ src/Components/Services/Information.js
@@ -25,7 +25,7 @@ class Information extends Component {
                             <p className='cb-paragraph font-size'>The GF Experience is a special package offered by our club that allows clients to relax and have 
                             a good time with our girls in a comfortable and casual setting. </p>
                             <p className='cb-paragraph font-size'>The experience is tailored to provide a range of options to suit different preferences. 
-                            Clients can choose to watch their favorite shows or movies on Netflix, engage in some fun and competitive Xbox gaming, or simply 
+                            Clients can choose to watch their favorite shows or movies, engage in some fun and competitive Xbox gaming, or simply 
                             engage in one-on-one conversations with our girls. The goal is to provide a fun and enjoyable experience for our clients and create 
                             an opportunity for them to connect with our girls in a more personal and engaging way.</p>
                         </div>
